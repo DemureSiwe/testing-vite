e0fc937833ae90534e5f2827063196ecad53e3ec
DemureBusi <letsetse.b@gmail.com>
2025-12-26 04:38:31 +0200

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,12 @@
 function ListGroup() {
-  const items = ["Johannesburg", "Durban", "Cape Town", "Limpompo", "Bloem"];
+  let items = ["Johannesburg", "Durban", "Cape Town", "Limpompo", "Bloem"];
+  items = [];
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
